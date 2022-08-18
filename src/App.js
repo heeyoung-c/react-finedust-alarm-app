fc73d0af88f1ce14e34a61ab07311df0715c18c0
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-08-19 00:29:43 +0900

    feat: Conf Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Route, Routes } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+import store from './modules'
+
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+
+import MyRegion from './pages/MyRegion'
+import EntireRegion from './pages/EntireRegion'
+import BookMark from './pages/BookMark'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div>
-      <p>프로젝트 시작</p>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path='/' element={<MyRegion />} />
+          <Route path='my-region' element={<MyRegion />} />
+          <Route path='entire-region' element={<EntireRegion />} />
+          <Route path='bookmark' element={<BookMark />} />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
+    </Provider>
   )
 }
 
