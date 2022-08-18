0b47580d75975e3d918da6a7d2f57aee980819e5
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-08-19 00:33:56 +0900

    feat: Create MyRegion page

---
+++ src/pages/MyRegion/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SelectSido from '../../components/SelectSido'
+import Card from '../../components/Card'
+
+const MyRegion = () => {
+  return (
+    <>
+      <SelectSido></SelectSido>
+      <Card></Card>
+    </>
+  )
+}
+
+export default MyRegion
