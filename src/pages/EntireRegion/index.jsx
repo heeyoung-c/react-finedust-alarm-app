6b28fa5be06e1c1f0decede55c0a8b160db09374
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-08-19 00:33:20 +0900

    feat: Create EntireRegion page

---
+++ src/pages/EntireRegion/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SelectSido from '../../components/SelectSido'
+
+const EntireRegion = () => {
+  return (
+    <div>
+      <SelectSido></SelectSido>
+    </div>
+  )
+}
+
+export default EntireRegion
