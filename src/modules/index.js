fef1a0bb56a0323180924449e2f8e6e2eb58908b
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-08-22 11:20:37 +0900

    conf: Add logger, ReduxThunk, combineReducers

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,24 @@
-import { legacy_createStore as createStore } from 'redux'
-import fineDust from './fineDust'
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  legacy_createStore as createStore,
+} from 'redux'
+import logger from 'redux-logger'
+import ReduxThunk from 'redux-thunk'
 
-const store = createStore(fineDust)
+import selectedValue from './selectedValue'
+import fineDustData from './fineDustData'
+
+const rootReducer = combineReducers({
+  fineDustData,
+  selectedValue,
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(ReduxThunk, logger)),
+)
 
 export default store
