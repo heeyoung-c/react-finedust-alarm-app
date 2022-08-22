7d6887e15f11694de4739fc26cddac68536bff32
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-08-22 11:19:41 +0900

    style: Modify theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,24 @@
 const palette = {
-  orange: '#FFA500',
-  black: '#000000',
+  primary: '#71C9CE',
+  secondary: '#A6E3E9',
+  tertiary: '#CBF1F5',
+  quaternary: '#E3FDFD',
+
+  stateGood: '#00CCFF',
+  stateNormal: '#33CC00',
+  stateNotBad: '#FF9900',
+  stateBad: '#FF3333',
+  stateWorst: '#993333',
+  stateUnKnown: '#999999',
+
+  white: '#FFFFFF',
 }
 
 const common = {
+  flexStart: `
+    display: flex;
+    align-items: center;
+  `,
   flexCenter: `
     display: flex;
     align-items: center;
