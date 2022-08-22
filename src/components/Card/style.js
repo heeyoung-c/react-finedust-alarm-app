461be22865e1891ea4caa1bc32b07605e0da9810
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-08-22 11:14:59 +0900

    style: Style in card

--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -2,26 +2,52 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   ${({ theme }) => theme.common.flexColumnStart};
-
-  border: 1px solid green;
+  font-family: 'NotoSansBold';
 `
 
 export const Card = styled.div`
   width: 80%;
-  height: 14rem;
-  border: 2px solid royalblue;
-  margin-bottom: 4rem;
+  margin-bottom: 8rem;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
+    rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
   font-size: ${({ theme }) => theme.fontSizes.title};
+  border-radius: 0.6rem;
+  background-color: ${({ pm10Grade }) =>
+    pm10Grade === '1'
+      ? ({ theme }) => theme.palette.stateGood
+      : pm10Grade === '2'
+      ? ({ theme }) => theme.palette.stateNormal
+      : pm10Grade === '3'
+      ? ({ theme }) => theme.palette.stateNotBad
+      : pm10Grade === '4'
+      ? ({ theme }) => theme.palette.stateBad
+      : pm10Grade === '5'
+      ? ({ theme }) => theme.palette.stateWorst
+      : ({ theme }) => theme.palette.stateUnKnown};
 `
 
 export const CardTop = styled.div`
-  border: 1px solid red;
+  ${({ theme }) => theme.common.flexStart};
+  color: ${({ theme }) => theme.palette.white};
+
+  margin: 1rem 1rem 0;
+`
+
+export const CardBookMark = styled.p`
+  margin-left: auto;
 `
 
 export const CardMiddle = styled.div`
-  border: 1px solid blue;
+  ${({ theme }) => theme.common.flexCenter};
+  color: ${({ theme }) => theme.palette.white};
+
+  margin: 0 4rem;
+  border: 2px dashed;
+  font-size: 5rem;
 `
 
 export const CardBottom = styled.div`
-  border: 1px solid green;
+  ${({ theme }) => theme.common.flexColumnStart};
+  color: ${({ theme }) => theme.palette.white};
+  margin-bottom: 1rem;
 `
