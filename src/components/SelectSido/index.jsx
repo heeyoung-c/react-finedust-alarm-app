637637781170e65af2b1c3c2291b1ff8814f6740
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-08-22 11:20:05 +0900

    feat: Modify selectedSido logic

--- src/components/SelectSido/index.jsx
+++ src/components/SelectSido/index.jsx
@@ -1,38 +1,51 @@
 import React from 'react'
 import * as S from './style'
 
-import SelectedStation from '../SelectedStation'
-import { SIDO_NAME_DATA } from '../../context/sidoNameData'
 import { useSelector, useDispatch } from 'react-redux'
-import { changeSelectedSido } from '../../modules/fineDust'
+import {
+  changeSelectedSido,
+  changeSelectedStation,
+  changeSelectedStations,
+} from '../../modules/selectedValue'
 
 const SelectSido = () => {
-  const selectedSido = useSelector(state => state.selectedSido)
+  const { selectedSido, sidoNameData } = useSelector(
+    state => state.selectedValue,
+  )
+  const { data: fineDustData } = useSelector(state => state.fineDustData)
   const dispatch = useDispatch()
 
-  const sidoChangeHandler = e => {
-    console.log('실행 1:', e.target.value)
-    dispatch(changeSelectedSido(e.target.value))
+  // 사용자가 지정한 시도 값이 바뀌면 그걸 저장하기 위한 로직
+  const changeSidoHandler = e => {
+    const selectedSido = e.target.value
+    dispatch(changeSelectedSido(selectedSido))
+
+    setStationsHandler(selectedSido)
+  }
+
+  // 세부 지역 바꾸기
+  const setStationsHandler = selectedSido => {
+    const selectedStations = fineDustData.filter(
+      item => item.sidoName === selectedSido,
+    )
+    dispatch(changeSelectedStations(selectedStations))
+    setStationHandler(selectedStations)
+  }
+
+  // 사용자가 세부지역을 설정 하지 않은 상태에서 첫번째 세부 지역을 자동으로 설정하여 저장)
+  const setStationHandler = selectedStations => {
+    dispatch(changeSelectedStation(selectedStations[0]))
   }
 
   return (
     <>
-      <S.Container>
-        <S.Select
-          size='lg'
-          id='sido-name'
-          name='sido-name'
-          onChange={sidoChangeHandler}
-        >
-          {SIDO_NAME_DATA.map(sido => (
-            <option key={sido.id} value={sido.value}>
-              {sido.value}
-            </option>
-          ))}
-        </S.Select>
-        <SelectedStation></SelectedStation>
-      </S.Container>
-      <p>{selectedSido}</p>
+      <S.Select defaultValue={selectedSido} onChange={changeSidoHandler}>
+        {sidoNameData.map(sido => (
+          <option key={sido.id} value={sido.value}>
+            {sido.value}
+          </option>
+        ))}
+      </S.Select>
     </>
   )
 }
--- src/components/SelectSido/style.js
+++ src/components/SelectSido/style.js
@@ -1,16 +1,10 @@
 import styled from 'styled-components'
 import { Form } from 'react-bootstrap'
 
-export const Container = styled.div`
-  height: 10rem;
-  border: 0.1rem solid blue;
-  ${({ theme }) => theme.common.flexCenter}
-`
-
 export const Select = styled(Form.Select)`
-  width: 10rem;
+  width: 12rem;
   height: 6rem;
-  border: 0.1rem solid red;
 
+  font-family: 'NotoSansBold';
   font-size: ${({ theme }) => theme.fontSizes.title};
 `
